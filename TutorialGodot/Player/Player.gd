198b6821f5d17b323069830ca521776270a30081
José Victor Gabriel <josevictor21112005@gmail.com>
2023-09-09 15:21:18 -0300

    Adicionando aceleração e fricção

--- TutorialGodot/Player/Player.gd
+++ TutorialGodot/Player/Player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+const ACCELERATION = 25
+const MAX_SPEED = 200
+const FRICTION = 25
 
 #velocidade padrao
 var velocity = Vector2.ZERO
@@ -9,11 +12,15 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
+	# Condicional que detecta se o jogador está pressionando as teclas para se mover
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
+		# Quando o jogador não estiver pressionando as teclas, ele irá desacelerar
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_collide(velocity)
 
