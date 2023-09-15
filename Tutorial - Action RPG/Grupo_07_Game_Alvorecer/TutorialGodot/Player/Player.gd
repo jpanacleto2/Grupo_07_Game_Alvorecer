5d4780c850fcec112580d363020ea7762a110a5b
ReiDavi23 <144080784+ReiDavi23@users.noreply.github.com>
2023-09-14 21:12:36 -0300

    Add files via upload

---
+++ Tutorial - Action RPG/Grupo_07_Game_Alvorecer/TutorialGodot/Player/Player.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
+#velocidade padrao
+var velocity = Vector2.ZERO
+
+
+#variável de acesso para animação de correr para os lados
+onready var animationPlayer = $AnimationPlayer
+
+	
+#"main"
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	# Condicional que detecta se o jogador está pressionando as teclas para se mover
+	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+			
+		
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		# Quando o jogador não estiver pressionando as teclas, ele irá desacelerar
+		animationPlayer.play("IdleRight")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	
+	velocity = move_and_slide(velocity)
+
+
