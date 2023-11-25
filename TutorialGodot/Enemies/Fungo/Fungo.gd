2926858e9933c3b3c187f4f25c472b6b5e907043
Pablo-R-L <99687142+Pablo-R-L@users.noreply.github.com>
2023-11-25 15:46:44 -0300

    Add files via upload

--- TutorialGodot/Enemies/Fungo/Fungo.gd
+++ TutorialGodot/Enemies/Fungo/Fungo.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var vida
-var cooldown
+var cooldown = true
 var agarraTempo
 var target
 var nextAttack
@@ -16,9 +16,10 @@ export var FRICTION = 200
 #se trocar de cenario tem q atualizar essa variavel manualmente
 onready var player = $"../../../Player"
 onready var wanderController = $WanderController
-onready var sprite = $Sprite
+onready var ani
 onready var stats = $Stats
 onready var hurtbox = $HurtBox
+onready var timer = $Timer
 
 var state = IDLE
 enum{
@@ -32,13 +33,15 @@ enum{
 func _process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	if Input.is_action_just_pressed("roll"):
+		state = CHASE
+	
 	match state:
 		IDLE:#so fica parado
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			if wanderController.get_time_left() == 0:
 				update_wander()
-			if Input.is_action_just_pressed("Abrir_Inv"):
-				state = CHASE
+			
 		
 		WANDER:#é apenas o modo wander do morcego, nada demais
 			if wanderController.get_time_left() == 0:
@@ -46,21 +49,15 @@ func _process(delta):
 			accelerate_towards_point(wanderController.target_position, delta)
 			if global_position.distance_to(wanderController.target_position) <= 4:
 				update_wander()#decide se fica parado ou n
-			if Input.is_action_just_pressed("Abrir_Inv"):
-				state = CHASE
-			
 		CHASE:
-			
-			sprite.texture = load("res://Enemies/Fungo/FungoCombate.png")
 			target = player.global_position - global_position
 			chase_state(delta)
 		ATTACK:
 			#Imagem: A-Train balofo
-			sprite.texture = load("res://Enemies/Fungo/FunguAttack.jpg")
+			
 			attack_state(delta)
 		LAUNCH:
 			#Imagem: cachorro quente
-			sprite.texture = load("res://Enemies/Fungo/FugusLaunch.jpg")
 			launch_state(delta)
 	velocity = move_and_slide(velocity)
 
@@ -73,22 +70,24 @@ func chase_state(delta):
 	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	
 	# 1 = ele se joga // 2 = ele bate
-	if nextAttack == 1:
+	if nextAttack == 1 && cooldown == true:
 		var distance = global_position.distance_to(player.global_position)
 		#se o ele estiver da distancia certa ele se joga
 		#se não, ele bate
-		if distance <= 80 && distance >= 4:
-			#se joga
+		if distance <= 80 && distance >= 4 :
+			timer.start(3)
 			state = LAUNCH
 		else:
 			#bate
 			nextAttack = 2
-	elif global_position.distance_to(player.global_position) <= 4:
+	elif global_position.distance_to(player.global_position) <= 4 && cooldown == true:
 		#se estiver perto o suficiente le bate
+		timer.start(3)
 		state = ATTACK
 
 func attack_state(delta):
 	#so pra eu saber q ele ta atacando
+	cooldown = false
 	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * 10 * delta)
 	if velocity == Vector2.ZERO:
 		#ja define o proximo ataque
@@ -97,6 +96,7 @@ func attack_state(delta):
 
 func launch_state(delta):
 	#so pra ver se ta tudo certo
+	cooldown = false
 	velocity = velocity.move_toward(target * MAX_SPEED, ACCELERATION * 2 * delta)
 	if global_position.distance_to(player.global_position) >= 100:
 		#define o proximo ataque
@@ -107,7 +107,6 @@ func launch_state(delta):
 func accelerate_towards_point(point, delta):
 	var direction = global_position.direction_to(point)
 	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-	sprite.flip_h = velocity.x < 0
 
 #decide se fica parado ou n
 func update_wander():
@@ -126,3 +125,7 @@ func _on_HurtBox_area_entered(area):
 	knockback = area.knockback_vector * 120
 	hurtbox.create_hit_effect()
 	hurtbox.start_invincibility(0.4)
+
+
+func _on_Timer_timeout():
+	cooldown = false
